145c5054b57c54c3e34fe502edf08f9b46ebf2e6
Yossig <yosigartner@gmail.com>
2019-12-20 12:56:58 +0200

    add mongodb connection

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Mongo connection successful')
+    })
+    .catch((err) => {
+        console.log(err);
+        console.log('Mongo connection error');
+    })
 
 app.listen(process.env.EXPRESS_PORT, (err) => {
     if (err) {
-      console.log(err);
-      process.exit(1);
+        console.log(err);
+        process.exit(1);
     }
     console.log(`server is running on port ${process.env.EXPRESS_PORT}`);
-  })
-  module.exports = app;
+})
+module.exports = app;
